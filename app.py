4309b8ac363277ab714dc8b153071308cef925e8
YINGHAIDADA <pyj2897022134@gmail.com>
2020-12-02 00:41:56 +0800

    添加表单提交后验证信息并跳转个人页面功能

--- app.py
+++ app.py
@@ -1,8 +1,22 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template,request
+from datetime import timedelta
 
 app = Flask(__name__)
 
-@app.route("/")
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = timedelta(seconds=1)
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(seconds=1)
+app.config['DEBUG'] = True
+
+@app.route("/",methods=["GET","POST"])
 def home():
+    if request.method == 'POST':
+        u = request.form['username']
+        p = request.form['password']
+        if u == '18115296' and p =='123456' :
+            return render_template("xinxi.html")
+
     return render_template("index.html")
+
+if __name__ == '__main__' :
+    app.run()
