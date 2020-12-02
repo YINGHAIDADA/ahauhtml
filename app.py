45822fea3e2727dc09e00d57c3443e5a7e8c924d
YINGHAIDADA <pyj2897022134@gmail.com>
2020-12-02 13:17:32 +0800

    修复新增页面的Logo不显示的问题

--- app.py
+++ app.py
@@ -1,42 +1,57 @@
-from flask import Flask, url_for,redirect
-from flask import render_template,request
+from flask import Flask, url_for, redirect, flash
+from flask import render_template, request
 from datetime import timedelta
 
 app = Flask(__name__)
 
+# -------------配置文件-----------
+app.config['SECRET_KEY'] = '123456'
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = timedelta(seconds=1)
 app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(seconds=1)
 app.config['DEBUG'] = True
 
-@app.route("/",methods=["GET","POST"])
+
+# -------------配置文件-----------
+
+
+@app.route("/", methods=["GET", "POST"])#指定接口访问的路径，以及请求方法
 def home():
     if request.method == 'POST':
+        error = None
         u = request.form['username']
         p = request.form['password']
-        if u == '18115296' and p =='123456' :
+        if u == '18115296' and p == '123456':
             return redirect(url_for('xinxi'))
-
+        else:
+            error = '用户名或密码错误'
+        flash(error)
     return render_template("index.html")
 
-@app.route("/xinxi",methods=["GET","POST"])
+
+@app.route("/xinxi", methods=["GET", "POST"])
 def xinxi():
     return render_template("xinxi.html")
+
 
 @app.route("/cailiaotijiao")
 def cailiaotijiao():
     return render_template("cailiaotijiao.html")
 
+
 @app.route("/shenhe")
 def shenhe():
     return render_template("shenhe.html")
+
 
 @app.route("/zongce")
 def zongce():
     return render_template("zongce.html")
 
+
 @app.route("/liuxin")
 def liuxin():
     return render_template("liuxin.html")
 
-if __name__ == '__main__' :
+
+if __name__ == '__main__':
     app.run()
